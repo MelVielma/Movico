1b0f4a2f2ccc517886cab6edb3aacc260f9e5a7f
Hector Slz <h.d.salazar@hotmail.com>
2019-11-20 20:26:44 -0600

    Desarrollo de busqueda de tags

--- src/containers/HomeEtiquetas.js
+++ src/containers/HomeEtiquetas.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyCardDeck from '../components/MyCardDeck';
+import MyNavbarWithoutTags from '../components/MyNavbarWithoutTags';
 
 class HomeEtiquetas extends React.Component{
   render(){
     console.log("Deberiamos de buscar: ", this.props.match.params.tags)
     return(
       <>
+        <div id="NavigationBar">
+          <MyNavbarWithoutTags />
+        </div>
         <div className="App">
           <MyCardDeck tags={this.props.match.params.tags} />
         </div>
--- src/containers/PublicationView.js
+++ src/containers/PublicationView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SinglePublicationView from '../components/SinglePublicationView'
+import SinglePublicationView from '../components/SinglePublicationView';
+import MyNavbar from '../components/MyNavbar';
 import '../index.css';
 import '../App.css';
 
@@ -8,6 +9,9 @@ class PublicationView extends React.Component{
   render(){
     return(
       <>
+        <div id="NavigationBar">
+          <MyNavbar />
+        </div>
         <div className="App">
            <SinglePublicationView pubId = {this.props.match.params.pubId} />
         </div>
