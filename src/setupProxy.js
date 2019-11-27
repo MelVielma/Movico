e959ac1a1b3ab73274a8fe33f04aa7303f3e3529
José Cruz <josecruz.9807@gmail.com>
2019-11-26 18:16:04 -0600

    Deploy test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", , "/otherApi"], { target: "https://movico.herokuapp.com" })
+    proxy(["/api", , "/"], { target: "https://movico.herokuapp.com" })
   );
 };
